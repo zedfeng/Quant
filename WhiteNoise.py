734cf54f9ade0ce95931c3c3a80fc82e4a2c5feb
Ziqiang Feng <ziqiang.nicholas.feng@gmail.com>
2016-11-07 12:15:16 +0000

    white noise

--- WhiteNoise.py
+++ WhiteNoise.py
@@ -3,12 +3,19 @@ white noise process
 """
 from random import normalvariate
 import matplotlib.pyplot as plt
-import numpy as np
 
-ts_len = 100
-noise = []
-for i in range(ts_len):
-    noise_val = normalvariate(0, 1)
-    noise.append(noise_val)
+
+def awgn(length, mean, var):
+    data = []
+    for i in range(length):
+        val = normalvariate(mean, var)
+        data.append(val)
+    return data
+
+
+noise_len = 100
+noise_mean = 0
+noise_var = 1
+noise = awgn(noise_len, noise_mean, noise_var)
 plt.plot(noise, '-b')
 plt.show()
