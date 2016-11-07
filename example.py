d74d5974ece83c7d6d5a39cba33053ed30c3e396
Ziqiang Feng <ziqiang.nicholas.feng@gmail.com>
2016-11-07 11:52:05 +0000

    white noise

--- example.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import scipy as sp
-data=sp.randn(5,2)
-dates=pd.date_range('28/12/2015',periods=5)
-df=pd.DataFrame(data,columns=('price','weight'),index=dates)
-print(df.mean())
