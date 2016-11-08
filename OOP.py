3ecd3eed057b378312aa7501254b548d0d89b096
Ziqiang Feng <ziqiang.nicholas.feng@gmail.com>
2016-11-08 14:47:19 +0000

    oop

--- OOP.py
+++ OOP.py
@@ -5,16 +5,17 @@ class ECDF:
     def __call__(self, x):
         return sum(1 for obs in self.observations if obs <= x) / len(self.observations)
 
-    class Polynomial:
-        def __init__(self, coefficients):
-            self.coefficients = coefficients
 
-        def __call__(self, x):
-            return sum(c * x ** i for i, c in enumerate(self.coefficients)
+class Polynomial:
+    def __init__(self, coefficients):
+        self.coefficients = coefficients
 
-        def differentiate(self):
-            diff_coefficients = []
-            for i, c in enumerate(self.coefficients):
-                diff_coefficients.append(i * c)
-            diff_coefficients.remove(0)
-            self.coefficients = diff_coefficients
+    def __call__(self, x):
+        return sum(c * x ** i for i, c in enumerate(self.coefficients))
+
+    def differentiate(self):
+        diff_coefficients = []
+        for i, c in enumerate(self.coefficients):
+            diff_coefficients.append(i * c)
+        diff_coefficients.remove(0)
+        self.coefficients = diff_coefficients
